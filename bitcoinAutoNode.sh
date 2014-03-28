4367f0c48f8babdcd4517d3b7832a07a66c47c52
Moe Adham <moeadham@gmail.com>
2014-03-28 00:21:19 -0400

    [Simple changes]

--- bitcoinAutoNode.sh
+++ bitcoinAutoNode.sh
@@ -3,18 +3,20 @@ echo "########### The server will reboot when the script is complete"
 echo "########### Changing to home dir"
 cd ~
 echo "########### Change your root password!"
-passwd
+#passwd
 echo "########### Firewall rules; allow 22,8333"
 ufw allow 22/tcp
 ufw allow 8333/tcp
+#port 789 for bitcoin rpc
+ufw allow 789/tcp
 ufw --force enable
 echo "########### Updating Ubuntu"
 apt-get update -y
 apt-get upgrade -y
 apt-get dist-upgrade -y
-apt-get install software-properties-common python-software-properties -y
+apt-get install software-properties-common python-software-properties haveged -y
 echo "########### Creating Swap"
-dd if=/dev/zero of=/swapfile bs=1M count=1024 ; mkswap /swapfile ; swapon /swapfile
+dd if=/dev/zero of=/swapfile bs=1M count=4096 ; mkswap /swapfile ; swapon /swapfile
 echo "/swapfile swap swap defaults 0 0" >> /etc/fstab
 echo "########### Adding ppa:bitcoin/bitcoin and installing bitcoind"
 add-apt-repository -y ppa:bitcoin/bitcoin
@@ -26,7 +28,8 @@ config=".bitcoin/bitcoin.conf"
 touch $config
 echo "server=1" > $config
 echo "daemon=1" >> $config
-echo "connections=40" >> $config
+echo "connections=100" >> $config
+echo "rpcport=789" >> $config
 randUser=`< /dev/urandom tr -dc A-Za-z0-9 | head -c30`
 randPass=`< /dev/urandom tr -dc A-Za-z0-9 | head -c30`
 echo "rpcuser=$randUser" >> $config
